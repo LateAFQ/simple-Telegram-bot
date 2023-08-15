9cbda163aeef4ac72558a371a3e11a853638a097
lateAFQ <ssergeenko821@gmail.com>
2023-08-15 17:30:58 +0300

    fix

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ img = open('./data/1.jpg', 'rb')
 # TODO: add english language
 @bot.message_handler(commands=['start'])
 def start_handler(message):
-    print('test commit')
     markup = types.InlineKeyboardMarkup(row_width=1)
     btn2 = types.InlineKeyboardButton("😻Питомцы в наличии",callback_data='main')
     btn3 = types.InlineKeyboardButton("📌Информация о нас", callback_data='info')
